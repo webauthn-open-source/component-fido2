88c6b275190a3d6b417d9fccc29f305f0e4abd99
Adam Powers <apowers@ato.ms>
2018-02-25 23:30:04 -0800

    half-working registration

--- test/mainTest.js
+++ test/mainTest.js
@@ -1,7 +1,10 @@
-var turnOnDebugLogging = false;
+var turnOnDebugLogging = true;
 
-var Fido2Component = require ("../index.js");
-var assert = require ("chai").assert;
+var Fido2Component = require("../index.js");
+var assert = require("chai").assert;
+const sinon = require("sinon");
+const helpers = require("fido2-helpers");
+
 
 var dummyComponentManager = {
     registerType: function() {},
@@ -23,25 +26,117 @@ var dummyLogger = {
         return new Proxy(function() {}, {
             get: function() {
                 return function(...msg) {
-                    if(turnOnDebugLogging) console.log(...msg);
+                    if (turnOnDebugLogging) console.log(...msg);
                 };
             },
         });
     }
 };
 
-describe ("cert tests", function() {
-    var c;
+describe("Fido2Component", function() {
+    var f2c;
     beforeEach(function() {
-        c = new Fido2Component(dummyComponentManager);
+        f2c = new Fido2Component(dummyComponentManager);
+        f2c.setServerDomain("localhost");
     });
 
     afterEach(function() {
-        c.shutdown();
+        f2c.shutdown();
     });
 
     it("can be initialized", function() {
-        var ret = c.init();
+        var ret = f2c.init();
         assert.isUndefined(ret);
+    });
+
+    describe("feature", function() {
+        it("setServerName", function() {
+            f2c.setServerDomain("example.com");
+            assert.strictEqual(f2c.serverDomain, "example.com");
+        });
+        it("getServerName", function() {
+            f2c.setServerDomain("example.com");
+            var ret = f2c.getServerDomain();
+            assert.strictEqual(ret, "example.com");
+        });
+        it("setServerName", function() {
+            f2c.setServerName("Facebook");
+            assert.strictEqual(f2c.serverName, "Facebook");
+        });
+        it("getServerName", function() {
+            f2c.setServerName("Google");
+            var ret = f2c.getServerName();
+            assert.strictEqual(ret, "Google");
+        });
+    });
+
+    describe("registerRequest", function() {
+        it.skip("resolves to a challenge", function(done) {
+            var args = [
+                null, {
+                    send: sendCb
+                }
+            ];
+
+            function sendCb(msg) {
+                assert.isString(msg);
+                msg = JSON.parse(msg);
+                console.log("msg", msg);
+                assert.isString(msg.challenge);
+                assert.strictEqual(msg.timeout, 60000);
+                assert.isObject(msg.rp);
+                assert.strictEqual(msg.rp.id, "example.com");
+                assert.strictEqual(msg.rp.name, "example.com");
+                done();
+            }
+
+            f2c.setServerDomain("example.com");
+            f2c.init();
+            f2c.registerRequest(...args);
+        });
+    });
+
+    describe("registerResponse", function() {
+        it("accepts a response", function(done) {
+            var body = helpers.copyObject(helpers.registerResponseMsg);
+            var args = [{
+                body: body
+            }, {
+                send: sendCb
+            }];
+
+            function sendCb() {
+                console.log ("sendCb");
+                done();
+            }
+
+            f2c.init();
+            f2c.registerResponse(...args);
+        });
+        it("errors when response is missing fields");
+    });
+
+    describe("loginRequest", function() {
+
+    });
+
+    describe("loginResponse", function() {
+
+    });
+
+    describe("setRegisterRequest", function() {
+
+    });
+
+    describe("setRegisterResponse", function() {
+
+    });
+
+    describe("setLoginRequest", function() {
+
+    });
+
+    describe("setLoginResponse", function() {
+
     });
 });
